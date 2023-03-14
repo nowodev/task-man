efde5b76c68aa341118afdfe9bf439c444662321
Favour <toluwaniokunowo@gmail.com>
2023-03-13 21:15:10 -0700

    generate task resources

---
+++ database/migrations/2023_03_14_040556_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Project;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignIdFor(Project::class)
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->string('name')->unique();
+            $table->integer('priority');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
