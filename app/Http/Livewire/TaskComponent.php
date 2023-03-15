49086ed87a904be1327c415e0e799fe5e30f1e4d
Favour <toluwaniokunowo@gmail.com>
2023-03-15 03:21:56 -0700

    move task form into seperate component

---
+++ app/Http/Livewire/TaskComponent.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Task;
+use Livewire\Component;
+
+class TaskComponent extends Component
+{
+    public $name;
+    public $task_id;
+    public $project;
+    public $priority;
+    public $tasks = [];
+    public $selected_project;
+    public $editing    = false;
+    public $createTask = false;
+
+    protected $listeners = ['refreshComponent', 'projectUpdated'];
+
+
+    public function mount($selected_project, $tasks)
+    {
+        $this->tasks            = $tasks;
+        $this->selected_project = $selected_project;
+    }
+
+    public function render()
+    {
+        return view('livewire.task-component');
+    }
+
+    public function submit()
+    {
+        $data = $this->validate([
+            'name'     => 'required|string',
+            'priority' => 'required',
+        ]);
+
+
+        if ($this->task_id) {
+            Task::find($this->task_id)->update($data);
+        } else {
+            Task::create(['project_id' => $this->selected_project] + $data);
+        }
+
+        $this->emit('refreshComponent');
+        $this->reset('name', 'priority', 'task_id');
+        $this->createTask = false;
+    }
+
+    public function edit($id)
+    {
+        $task          = Task::find($id);
+        $this->task_id = $id;
+
+        $this->editing    = true;
+        $this->createTask = true;
+        $this->name       = $task->name;
+        $this->priority   = $task->priority;
+    }
+
+    public function delete($id)
+    {
+        Task::find($id)->delete();
+        $this->emit('refreshComponent');
+    }
+
+    public function refreshComponent()
+    {
+        $this->tasks = Task::where('project_id', $this->selected_project)->get();
+    }
+
+    public function projectUpdated($value)
+    {
+        $this->selected_project = $value;
+        $this->tasks            = Task::where('project_id', $value)->get();
+    }
+}
